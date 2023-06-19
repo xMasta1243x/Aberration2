df848ecf18f3b0717b2605e1409fcc351c25cd07
xMasta1243x <rodrigoyessiz@gmail.com>
2023-06-19 16:37:08 -0400

    firstpublish

---
+++ Assets/Script/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private bool isJumping = false;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Movimiento horizontal
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+
+        // Flip del sprite
+        if (moveX < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveX > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verificar si el objeto ha tocado el suelo
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+}
+
